ed1ccda5601a863e94ed8e5bfc53f0d0ebfffa16
Rogelio Mendiola Jr <rogeliojr1234594@gmail.com>
2023-06-02 22:37:04 +0800

    Create user component

---
+++ src/app/mock-users.ts
@@ -0,0 +1,31 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+    {
+        id: 1,
+        firstname: 'Tony',
+        lastname: 'Stark',
+        username: 'ironman',
+        email: 'ironman@marvel.com',
+        password: 'iamironman',
+        lastlogin: 'January 1, 2011'
+    },
+    {
+        id: 1,
+        firstname: 'Bruce',
+        lastname: 'Banner',
+        username: 'hulk',
+        email: 'hulk@marvel.com',
+        password: 'hulksmash',
+        lastlogin: 'February 1, 2011'
+    },
+    {
+        id: 1,
+        firstname: 'Nick',
+        lastname: 'Fury',
+        username: 'fury',
+        email: 'fury@marvel.com',
+        password: 'iseeyou',
+        lastlogin: 'March 1, 2011'
+    } 
+];
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { User } from '../user';
+import { USERS } from '../mock-users';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+  users = USERS;
+}
