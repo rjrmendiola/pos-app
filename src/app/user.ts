ef86f3106f71420d58564627f3500e344d5028b0
Rogelio Mendiola Jr <rogeliojr1234594@gmail.com>
2023-06-04 23:22:08 +0800

    Implement user creation and backend integration

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,8 +2,9 @@ export interface User {
     id: number;
     firstname: string;
     lastname: string;
+    middlename: string;
     username: string;
     email: string;
     password: string;
-    lastlogin: string;
+    lastlogin?: string;
   }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,12 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 import { User } from '../user';
-import { USERS } from '../mock-users';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
-  users = USERS;
+export class UsersComponent implements OnInit {
+  users: User[] = [];
+  selectedUser: User | undefined;
+  // By using the "!" non-null assertion operator, 
+  // we inform TypeScript that the userForm property will be initialized in the initUserForm() method.
+  userForm!: FormGroup;
+  deleteUserId: number | undefined;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService, 
+    private modalService: NgbModal
+  ) {}
+
+  ngOnInit(): void {
+    this.initUserForm();
+    this.getUsers();
+  }
+
+  initUserForm(): void {
+    this.userForm = this.formBuilder.group({
+      id: [''],
+      firstname: ['', Validators.required],
+      middlename: [''],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  getUsers(): void {
+    this.userService.getUsers()
+      .subscribe(users => this.users = users);
+  }
+
+  openModal(content: any, user?: User): void {
+    this.selectedUser = user ? { ...user } : undefined;
+    if (this.selectedUser) {
+      // Exclude password field during edit
+      const { password, ...userWithoutPassword } = this.selectedUser;
+      this.userForm.reset(userWithoutPassword);
+    } else {
+      this.userForm.reset();
+    }
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+
+  openDeleteConfirmationModal(content: any, userId: number): void {
+    this.deleteUserId = userId;
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+  
+  saveUser(user: User): void {
+    // Mark form controls as touched
+    Object.keys(this.userForm.controls).forEach(key => {
+      this.userForm.get(key)?.markAsTouched();
+    });
+
+    if (this.userForm.invalid) {
+      return;
+    }
+
+    if (user.id) {
+      this.userService.updateUser(user)
+        .subscribe(() => {
+          this.getUsers();
+          this.modalService.dismissAll();
+        });
+    } else {
+      this.userService.createUser(user)
+        .subscribe(() => {
+          this.getUsers();
+          this.modalService.dismissAll();
+        });
+    }
+  }
+
+  deleteUser(): void {
+    if (this.deleteUserId) {
+      this.userService.deleteUser(this.deleteUserId)
+        .subscribe(() => {
+          this.getUsers();
+          this.modalService.dismissAll();
+        });
+    }
+  }
 }
